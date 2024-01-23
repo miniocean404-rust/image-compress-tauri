4e0df551b7c258954a2378cb6d46d332210c8df8
MiniOcean404 Davinci <haiyang.dai@davincimotor.com>
2024-01-23 11:46:53 +0800

    初始化模板

---
+++ src-tauri/src/main.rs
@@ -0,0 +1,20 @@
+// Prevents additional console window on Windows in release, DO NOT REMOVE!!
+#![cfg_attr(not(debug_assertions), windows_subsystem = "windows")]
+
+use image_compress_tauri::{
+    __cmd__close_splashscreen, __cmd__greet,
+    command::custom::{close_splashscreen, greet},
+    menu::{entry::get_menu, event::menu_event},
+};
+
+fn main() {
+    tauri::Builder::default()
+        // 注册命令、方法
+        .invoke_handler(tauri::generate_handler![greet, close_splashscreen])
+        // 添加菜单
+        .menu(get_menu())
+        // 监听自定义菜单事件
+        .on_menu_event(menu_event())
+        .run(tauri::generate_context!())
+        .expect("error while running tauri application");
+}
---
+++ src/main.tsx
@@ -0,0 +1,19 @@
+import React from "react";
+import ReactDOM from "react-dom/client";
+import App from "./App";
+import "./styles.css";
+
+import { invoke } from '@tauri-apps/api/tauri'
+
+// DOM 内容加载完成之后，通过 invoke 调用 在 Rust 中已经注册的命令
+window.addEventListener('DOMContentLoaded', () => {
+  setTimeout(async () => {
+    await invoke('close_splashscreen')
+  }, 1000)
+})
+
+ReactDOM.createRoot(document.getElementById("root") as HTMLElement).render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>,
+);
