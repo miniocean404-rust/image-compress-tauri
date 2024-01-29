58f0a16f767dcac54dad02033148621b0e4936c0
MiniOcean404 Davinci <haiyang.dai@davincimotor.com>
2024-01-29 14:07:20 +0800

    feat: 修改主线程 tokio

--- src-tauri/src/main.rs
+++ src-tauri/src/main.rs
@@ -18,21 +18,17 @@ use image_compress_tauri::{
 };
 
 fn main() -> Result<(), Box<dyn Error>> {
-    let rt = tokio::runtime::Builder::new_multi_thread()
-        // 开启所有特性
-        .enable_all()
-        // 监听线程停止
-        .on_thread_stop(async_thread_stop)
-        // 构建 runtime
-        .build()?;
+    // let rt = tokio::runtime::Builder::new_multi_thread()
+    //     // 开启所有特性
+    //     .enable_all()
+    //     // 监听线程停止
+    //     .on_thread_stop(async_thread_stop)
+    //     // 构建 runtime
+    //     .build()?;
 
-    // 等价于 #[tokio::main()]
-    rt.block_on(async_main())?;
+    // // 等价于 #[tokio::main()]
+    // rt.block_on(async_main())?;
 
-    Ok(())
-}
-
-async fn async_main() -> Result<(), Box<dyn Error>> {
     let _guard = init_tracing()?;
 
     tauri::Builder::default()
@@ -50,6 +46,8 @@ async fn async_main() -> Result<(), Box<dyn Error>> {
 
     Ok(())
 }
+
+async fn async_main() -> Result<(), Box<dyn Error>> {}
 
 fn async_thread_stop() {
     // warn!("异步线程停止了");
