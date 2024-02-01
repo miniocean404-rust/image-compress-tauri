0f46e3b1921a342c92e55ad9f140d758b5245bfd
MiniOcean404 Davinci <haiyang.dai@davincimotor.com>
2024-02-01 11:41:31 +0800

    feat: 优化 jpeg 代码

--- src-tauri/src/compress/core/jpeg/lib_mozjpeg_sys/lossless.rs
+++ src-tauri/src/compress/core/jpeg/lib_mozjpeg_sys/lossless.rs
@@ -1,96 +1,86 @@
-#![feature(c_unwind)]
-#![allow(unused_attributes)]
-
-use std::error::Error;
-use std::ffi::c_void;
-
-use std::fs::File;
-use std::io::Write;
-use std::panic::catch_unwind;
-use std::{fs, mem, ptr};
-
-use libc::free;
 use mozjpeg_sys::*;
+use std::{mem, ptr, slice};
 
-use super::common::{error_handler, error_message_handler, write_metadata};
+use super::common::write_metadata;
 
-pub fn to_mem(input: &str) -> Result<Vec<u8>, Box<dyn Error>> {
-    let in_file = fs::read(input)?;
-
-    unsafe { catch_unwind(|| -> Result<Vec<u8>, Box<dyn Error>> { lossless(in_file, false) }).expect("执行 jpeg_sys panic 了") }
+struct JPEGOptimizer {
+    srcinfo: jpeg_decompress_struct,
+    dstinfo: jpeg_compress_struct,
 }
 
-pub fn to_file(input: &str, output: &str) -> Result<(), Box<dyn std::error::Error>> {
-    let out_buffer = to_mem(input)?;
-    let mut out_file = File::create(output)?;
-    out_file.write_all(&out_buffer)?;
-
-    Ok(())
-}
-
-/// # Safety
-pub unsafe fn lossless(mem: Vec<u8>, keep_metadata: bool) -> Result<Vec<u8>, Box<dyn std::error::Error>> {
-    // 创建压缩数据指针
-    let mut decompress_info: jpeg_decompress_struct = mem::zeroed();
-    let mut compress_info: jpeg_compress_struct = mem::zeroed();
-
-    // 设置解压缩错图提示
-    let mut decompress_err = mem::zeroed();
-    decompress_info.common.err = jpeg_std_error(&mut decompress_err);
-    (*decompress_info.common.err).error_exit = Some(error_handler);
-    (*decompress_info.common.err).output_message = Some(error_message_handler);
-
-    // 设置压缩错误提示
-    let mut compress_err = mem::zeroed();
-    compress_info.common.err = jpeg_std_error(&mut compress_err);
-    (*compress_info.common.err).error_exit = Some(error_handler);
-    (*compress_info.common.err).output_message = Some(error_message_handler);
-
-    // 初始化压缩解压缩
-    jpeg_create_decompress(&mut decompress_info);
-    jpeg_create_compress(&mut compress_info);
-
-    // 设置解压缩 vec 数据到 decompress_info
-    jpeg_mem_src(&mut decompress_info, mem.as_ptr(), mem.len() as c_ulong);
-
-    if keep_metadata {
-        jpeg_save_markers(&mut decompress_info, 0xFE, 0xFFFF);
-        for m in 0..16 {
-            jpeg_save_markers(&mut decompress_info, 0xE0 + m, 0xFFFF);
+impl JPEGOptimizer {
+    unsafe fn new() -> JPEGOptimizer {
+        JPEGOptimizer {
+            srcinfo: mem::zeroed(),
+            dstinfo: mem::zeroed(),
         }
     }
-
-    jpeg_read_header(&mut decompress_info, true as boolean);
-
-    // 读取系数
-    let src_coef_arrays = jpeg_read_coefficients(&mut decompress_info);
-    jpeg_copy_critical_parameters(&decompress_info, &mut compress_info);
-    let dst_coef_arrays = src_coef_arrays;
-
-    // 设置优化编码
-    compress_info.optimize_coding = i32::from(true);
-
-    // 开始写入 buf 及系数
-    let mut buf = ptr::null_mut();
-    let mut buf_size = 0;
-    jpeg_mem_dest(&mut compress_info, &mut buf, &mut buf_size);
-    jpeg_write_coefficients(&mut compress_info, dst_coef_arrays);
-
-    if keep_metadata {
-        write_metadata(&mut decompress_info, &mut compress_info);
-    }
-
-    // 完成压缩
-    jpeg_finish_compress(&mut compress_info);
-    jpeg_destroy_compress(&mut compress_info);
-    jpeg_finish_decompress(&mut decompress_info);
-    jpeg_destroy_decompress(&mut decompress_info);
-
-    let slice = std::slice::from_raw_parts(buf, buf_size as usize);
-
-    let result = slice.to_vec();
-
-    free(buf as *mut c_void);
-
-    Ok(result)
 }
+
+impl Drop for JPEGOptimizer {
+    fn drop(&mut self) {
+        unsafe {
+            jpeg_destroy_decompress(&mut self.srcinfo);
+            jpeg_destroy_compress(&mut self.dstinfo);
+        }
+    }
+}
+
+// https://github.com/imazen/imageflow/blob/9ca1dbd50c7390d5953d711bf78b49ea5217aa60/imageflow_core/src/codecs/mozjpeg_decoder.rs#L17
+// This function losslessly optimizes jpegs.
+// Based on the jpegtran.c example program in libjpeg.
+pub fn optimize_lossless_jpeg(bytes: &[u8], keep_metadata: bool) -> std::thread::Result<&mut [u8]> {
+    unsafe {
+        std::panic::catch_unwind(|| {
+            let mut info = JPEGOptimizer::new();
+            let mut err = create_error_handler();
+            info.srcinfo.common.err = &mut err;
+            jpeg_create_decompress(&mut info.srcinfo);
+            jpeg_mem_src(&mut info.srcinfo, bytes.as_ptr(), bytes.len() as c_ulong);
+
+            // 原本没有
+            if keep_metadata {
+                jpeg_save_markers(&mut info.srcinfo, 0xFE, 0xFFFF);
+                for m in 0..16 {
+                    jpeg_save_markers(&mut info.srcinfo, 0xE0 + m, 0xFFFF);
+                }
+            }
+
+            info.dstinfo.optimize_coding = 1;
+            info.dstinfo.common.err = &mut err;
+            jpeg_create_compress(&mut info.dstinfo);
+            jpeg_read_header(&mut info.srcinfo, 1);
+
+            let src_coef_arrays = jpeg_read_coefficients(&mut info.srcinfo);
+            jpeg_copy_critical_parameters(&info.srcinfo, &mut info.dstinfo);
+
+            let mut buf = ptr::null_mut();
+            let mut outsize: c_ulong = 0;
+
+            jpeg_mem_dest(&mut info.dstinfo, &mut buf, &mut outsize);
+            // jpeg_set_quality(&mut info.dstinfo, 80, false as boolean);
+            jpeg_write_coefficients(&mut info.dstinfo, src_coef_arrays);
+
+            if keep_metadata {
+                write_metadata(&mut info.srcinfo, &mut info.dstinfo);
+            }
+
+            jpeg_finish_compress(&mut info.dstinfo);
+            jpeg_finish_decompress(&mut info.srcinfo);
+
+            slice::from_raw_parts_mut(buf, outsize as usize)
+        })
+    }
+}
+
+unsafe fn create_error_handler() -> jpeg_error_mgr {
+    let mut err: jpeg_error_mgr = mem::zeroed();
+    jpeg_std_error(&mut err);
+    err.error_exit = Some(unwind_error_exit);
+    err.emit_message = Some(silence_message);
+    err
+}
+
+extern "C-unwind" fn unwind_error_exit(cinfo: &mut jpeg_common_struct) {}
+
+extern "C-unwind" fn silence_message(_cinfo: &mut jpeg_common_struct, _level: c_int) {}
